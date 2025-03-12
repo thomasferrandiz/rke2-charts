3e1a9966421899ff943d0942847a71f60d60c5ab
Thomas Ferrandiz <thomas.ferrandiz@suse.com>
2025-03-12 10:06:24 +0000

    TEST homedir

--- updatecli/scripts/update-coredns.sh
+++ updatecli/scripts/update-coredns.sh
@@ -6,6 +6,8 @@ source $(dirname $0)/create-issue.sh
 ISSUE_TITLE="Updatecli failed for coredns ${COREDNS_VERSION}" 
 trap report-error EXIT INT
 
+HOMEDIR="/home/azureuser"
+
 if [ -n "$COREDNS_VERSION" ]; then
 	coredns_url=$(yq '.url' packages/rke2-coredns/package.yaml)
 	current_coredns_version=$(awk -F'/coredns-|.tgz' '{print $2}' <<< $coredns_url)
@@ -13,9 +15,9 @@ if [ -n "$COREDNS_VERSION" ]; then
 		echo "Updating Coredns chart to $COREDNS_VERSION"
 		yq -i ".url = \"https://github.com/coredns/helm/releases/download/coredns-${COREDNS_VERSION}/coredns-${COREDNS_VERSION}.tgz\" |
 			.packageVersion = 00" packages/rke2-coredns/package.yaml
-		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-coredns' make prepare
+		GOCACHE='${HOMEDIR}/.cache/go-build' GOPATH='${HOMEDIR}/go' PACKAGE='rke2-coredns' make prepare
 		find packages/rke2-coredns/charts -name '*.orig' -delete
-		GOCACHE='/home/runner/.cache/go-build' GOPATH='/home/runner/go' PACKAGE='rke2-coredns' make patch
+		GOCACHE='${HOMEDIR}/.cache/go-build' GOPATH='${HOMEDIR}/go' PACKAGE='rke2-coredns' make patch
 		make clean
 	fi
 fi
